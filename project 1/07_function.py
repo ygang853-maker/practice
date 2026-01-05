196b2bb69c0f536df144b4c8aa1cd7c15b4e2642
young <ygang853@gmail.com>
2026-01-05 09:25:53 +0900

    2026-01-05

---
+++ project 1/07_function.py
@@ -0,0 +1,9 @@
+# 1. 괄호 안에 'name'이라는 변수를 뚫어줌
+def hello_friend(name):
+    print(f"안녕 난 {name} 이야")
+    print(f"{name} 라는 이름 어때?")
+
+# 2. 괄호 안에 값 넣기
+hello_friend("멜리나")
+print("--------------------")
+hello_friend("라니")
